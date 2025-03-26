3452cc6551555cf24304523fa30238636d92de51
Brobece <74504989+Brobece@users.noreply.github.com>
2025-03-26 19:43:56 -0300

    Update dicionarios.py

--- dicionarios.py
+++ dicionarios.py
@@ -21,4 +21,23 @@ print(len(aluno))
 
 #verificar a existencia de um par de chave1
 print("idade" in aluno)
-print("alutra" in aluno)
+print("altura" in aluno)
+
+#A função “ get()” é muito útil para evitar erros de chave inexistente.
+print(aluno.get("Idade"))
+print(aluno.get("Peso"))
+
+#A função “items()” retorna uma lista de tuplas com os pares “chave” : valor.
+print(aluno.items())
+#A função “keys()” retorna uma lista apenas com as chaves do dicionário.
+print(aluno.keys())
+#A função “values()” retorna uma lista apenas com os valores dos itens do dicionário:
+print(aluno.values())
+
+#atualizando o dicionario
+aluno = {"Nome": "João GUilherme", "Idade": 18, "Nota": 9.9}
+aluno_update = {"Peso": 70, "Nota": 8.5
+aluno.update(aluno_update)
+print(aluno_update)
+
+#sets: Os sets são listas ou coleções não ordenadas de itens únicos.                
